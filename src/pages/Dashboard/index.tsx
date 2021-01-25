b7e0d175d31f00ebb8912f8facd926a3bc7c5bc8
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-01-25 17:28:08 -0300

    feat: added handleAddRepository function

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 
 import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/images/logo.svg';
+import api from '../../service/api';
 import { Form, Repositories, Title } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`/repos/${newRepo}`);
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
   return (
     <>
       <img src={logoImg} alt="GitHub Explorer" />
       <Title>Dashboard</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+        />
         <button type="submit">PESQUISAR</button>
       </Form>
 
