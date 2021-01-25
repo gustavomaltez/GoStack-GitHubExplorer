014067fe5e71a45eb515865514c7c311011450bd
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-01-25 16:58:17 -0300

    styles: added repository item

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import logoImg from '../../assets/images/logo.svg';
-import { Form, Title } from './styles';
+import { Form, Repositories, Title } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -13,6 +13,20 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do repositório" />
         <button type="submit">PESQUISAR</button>
       </Form>
+
+      <Repositories>
+        <a href="/">
+          <img
+            src="https://avatars.githubusercontent.com/u/57905553?s=460&u=370ce9e5f08ca8e021cd51ee84e3ba5a82105f89&v=4"
+            alt="Gustavo Maltez"
+          />
+
+          <div>
+            <strong>Example</strong>
+            <p>Repository Description</p>
+          </div>
+        </a>
+      </Repositories>
     </>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -42,3 +42,41 @@ export const Form = styled.form`
     }
   }
 `;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+  }
+`;
